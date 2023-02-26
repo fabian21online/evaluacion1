56cf3d978d3c4bceffedc89f64ae53851185442d
Fabian <fabnusa2002@gmail.com>
2023-02-26 06:07:31 -0500

    se agrega el proyecto

---
+++ src/main/java/bean/Pelicula.java
@@ -0,0 +1,102 @@
+package bean;
+
+public class Pelicula {
+	
+	int id, idClasificacion,idEstado;
+ String Titulo,FechaEstreno,Director,Generos,Duracion,link,Reparto,Sinopsis;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public int getIdClasificacion() {
+	return idClasificacion;
+}
+public void setIdClasificacion(int idClasificacion) {
+	this.idClasificacion = idClasificacion;
+}
+public int getIdEstado() {
+	return idEstado;
+}
+public void setIdEstado(int idEstado) {
+	this.idEstado = idEstado;
+}
+public String getTitulo() {
+	return Titulo;
+}
+public void setTitulo(String titulo) {
+	Titulo = titulo;
+}
+public String getFechaEstreno() {
+	return FechaEstreno;
+}
+public void setFechaEstreno(String fechaEstreno) {
+	FechaEstreno = fechaEstreno;
+}
+public String getDirector() {
+	return Director;
+}
+public void setDirector(String director) {
+	Director = director;
+}
+public String getGeneros() {
+	return Generos;
+}
+public void setGeneros(String generos) {
+	Generos = generos;
+}
+public String getDuracion() {
+	return Duracion;
+}
+public void setDuracion(String duracion) {
+	Duracion = duracion;
+}
+public String getLik() {
+	return link;
+}
+public void setLik(String lik) {
+	this.link = lik;
+}
+public String getReparto() {
+	return Reparto;
+}
+public void setReparto(String reparto) {
+	Reparto = reparto;
+}
+public String getSinopsis() {
+	return Sinopsis;
+}
+public void setSinopsis(String sinopsis) {
+	Sinopsis = sinopsis;
+}
+ 
+ public Pelicula(String[]aRegistro) {
+	 
+	 if (aRegistro==null) return;
+	 
+	 id = Integer.parseInt(aRegistro[0]);
+	 Titulo = aRegistro[0];
+	 
+	 if (aRegistro.length==4) {
+		 
+		 Sinopsis = aRegistro[2];
+		 link = aRegistro[3];
+		 return;
+	 }
+	 
+	 FechaEstreno = aRegistro[2];
+	 Director = aRegistro[3];
+	 Generos  = aRegistro[4];
+	 idClasificacion = Integer.parseInt(aRegistro[5]);
+	 idEstado = Integer.parseInt(aRegistro[6]);
+	 Duracion = aRegistro[7];
+	 link = aRegistro[8];
+	 Reparto = aRegistro[9];
+	 Sinopsis = aRegistro[10];
+	 
+	 idClasificacion = Integer.parseInt(aRegistro[0]);
+	 idEstado = Integer.parseInt(aRegistro[0]);
+ }
+ 
+ }
---
+++ src/main/webapp/WEB-INF/footer.jsp
@@ -0,0 +1,7 @@
+<%@ page pageEncoding="ISO-8859-1"%>
+
+<footer>
+			<div class="contenido-footer">
+				<p>Copyright © 2016 ® Multicines CINESTAR - Todos los Derechos Reservados</p>
+			</div>
+		</fo oter>
